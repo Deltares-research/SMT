abc7e86785e25f12162161dee63c9b2a798b342a
Willem Ottevanger <willem4@github.com>
2020-04-29 07:54:10 +0000

    added clean option, and option to skip previously computed model runs
    git-svn-id: https://repos.deltares.nl/repos/RIVmodels/rivtools/trunk/python/smt@1166 da8592f4-ba6a-403f-8d83-fa801fd319dd

--- src/runsim.py
+++ src/runsim.py
@@ -32,7 +32,8 @@ def print_version(ctx, param, value):
 @click.option('-v', '--version', is_flag=True, callback=print_version,
               expose_value=False, is_eager=True, help='Print version information')
 @click.option('-s', '--settings', default='smt.yml', help='SMT settings YAML file')
-def runner(settings): 
+@click.option('-c', '--clean', is_flag=False, help='Indicates whether previous output should be cleaned')
+def runner(settings, clean): 
     # create logger
     logger = tools.init_logger()
 
@@ -42,24 +43,33 @@ def runner(settings):
     # check input 
     model.validate(smt_settings)
 
+    # clean previous simulation 
+    if clean: 
+        logger.info(f'Cleaning previous output')
+        tools.remove('output')
+
     # get model input 
     for model_settings in model.get_input(smt_settings): 
+        # check if previous output exists 
+        new_output_folder = os.path.join('output', str(model_settings['TStop']))
+        if os.path.exists(new_output_folder): 
+            logger.info(f'Output folder {new_output_folder} exists, skipping ...')
+            continue
+
         # apply input 
         tools.guaranteedir('work')
         tools.copy('source','work')
-
-        print(model_settings)
-        
         model.adapt(model_settings, smt_settings)
         tools.remove('work/**.template')
    
         # run model
         app = Application(run_script=smt_settings['application']['command'])
-        app.run('work', smt_settings['model'])
+        app.run('work', smt_settings['model']['input'])
 
         # finalize model 
-        tools.guaranteedir('work')
-        tools.move('work', str(model_settings['TStop']))
+        tools.guaranteedir('output')
+        tools.guaranteedir(new_output_folder)
+        tools.move('work', new_output_folder)
 
 if __name__ == '__main__':
     runner()
