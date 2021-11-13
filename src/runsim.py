4625756abf9ca89d4f4132a97eac0e89a9a4e7a4
Willem Ottevanger <willem4@github.com>
2021-11-13 06:54:35 +0000

    git-svn-id: https://repos.deltares.nl/repos/RIVmodels/rivtools/trunk/python/smt@1698 da8592f4-ba6a-403f-8d83-fa801fd319dd

--- src/runsim.py
+++ src/runsim.py
@@ -19,7 +19,7 @@ def print_version(ctx, param, value):
     import netCDF4
     if not value or ctx.resilient_parsing:
         return
-    click.echo('SMT version 2.0.$Rev$')  #TODO set version number
+    click.echo('SMT version 2.0.$Revision$')  #TODO set version number
     click.echo('--dependencies---')
     click.echo(f'click : {click.__version__}')
     click.echo(f'logging: {logging.__version__}')
