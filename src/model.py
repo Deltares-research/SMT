4d448e6411e6a125e0b3ed35e9a5d0aae0e14dde
Willem Ottevanger <willem4@github.com>
2023-12-12 15:44:21 +0000

    add Tlfsmo option
    git-svn-id: https://repos.deltares.nl/repos/RIVmodels/rivtools/trunk/python/smt@2267 da8592f4-ba6a-403f-8d83-fa801fd319dd

--- src/model.py
+++ src/model.py
@@ -268,7 +268,9 @@ def get_input(smt_settings):
                 if 'MapOutputCount' not in model_settings.keys():
                     model_settings['MapOutputCount'] = 1
                 if 'Dtfacmax' not in model_settings.keys():
-                    model_settings['Dtfacmax'] = 1.1                    
+                    model_settings['Dtfacmax'] = 1.1     
+                if 'Tlfsmo' not in model_settings.keys():
+                    model_settings['Tlfsmo'] = 0.0       
                 # At this point we have read the desired SpinupTime and TimeDuration of the morphodynamic activity. 
                 # Now we have to find a DtUserModel which is close to the desired DtUser and allows for the setting of different DtUser related outputs
                 #
