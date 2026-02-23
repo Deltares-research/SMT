77d872f273be93eecb2d0354adeaaa8c4fb07779
Willem Ottevanger <willem.ottevanger@deltares.nl>
2026-02-23 08:44:23 +0100

    update version output

--- runsim.py
+++ runsim.py
@@ -6,6 +6,7 @@ import mako
 import os
 import platform
 import sys 
+import scipy
 import yaml 
 import shutil
 
@@ -16,15 +17,33 @@ from application import Application
 
 def print_version(ctx, param, value):
     import netCDF4
+    import subprocess
+    import importlib
     if not value or ctx.resilient_parsing:
         return
-    click.echo('SMT version 2.0.' + '$Revision: 2303 $'.split(' ')[1])
+    try:
+        git_version = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], stderr=subprocess.DEVNULL).decode().strip()
+    except Exception:
+        git_version = 'unknown'
+    # Versioning follows semantic versioning: https://semver.org/ - major.minor.patch
+    # MAJOR version when you make incompatible API changes
+    # MINOR version when you add functionality in a backward compatible manner
+    # PATCH version when you make backward compatible bug fixes
+    click.echo(f'SMT version 2.1.1 (git commit: {git_version})')
+
+    # Print versions of dependencies
     click.echo('--dependencies---')
-    click.echo(f'click : {click.__version__}')
+    click.echo(f'click : {importlib.metadata.version("click")}')
     click.echo(f'logging: {logging.__version__}')
     click.echo(f'mako: {mako.__version__}')  
     click.echo(f'netCDF4: {netCDF4.__version__}')  
     click.echo(f'yaml: {yaml.__version__}')  
+    # Optional dependency - used for refplane.py
+    try:
+        import scipy
+        click.echo(f'scipy: {scipy.__version__}')
+    except ImportError as e:
+        pass
     ctx.exit()
 
 @click.command()
