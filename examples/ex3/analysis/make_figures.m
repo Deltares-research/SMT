ffd3664243459fb52c234ff12e47407f518f3684
Willem Ottevanger <willem.ottevanger@deltares.nl>
2026-05-14 06:53:49 +0200

    clean up comments and add attribution

--- examples/ex3/analysis/make_figures.m
+++ examples/ex3/analysis/make_figures.m
@@ -119,21 +119,6 @@ T.error_L1 = error_L1;
 T.error_Linf = error_Linf; 
 T.error_L2 = error_L2; 
 
-%
-
-%N = 20; 
-% 
-%dx = L/N;
-
-% sim_count = 4;
-% dx = NaN*ones(sim_count,1);
-% error_L1 = NaN*ones(sim_count,1);
-% error_Linf = NaN*ones(sim_count,1);
-% 
-% make_each_plot = true;
-
-
-%D3D_grd2map('c:\projects\_temp\masscon\r037\1x20_net.nc', 'fpath_exe', 'c:\Program Files\Deltares\Delft3D FM Suite 2026.01 HMWQ\plugins\DeltaShell.Dimr\kernels\x64\bin\run_dimr.bat');
 for k = 1:length(select) % sim_count 
     fprintf('Processing plot %i\n', k);
     figure(graph{k});
@@ -162,91 +147,11 @@ for f = 1:length(figs);
     print('-r300','-dpng', graph_output{k});
 end
 
-    %set(gca, 'x')
-% caption{k} = 'Explicit';
-% x_label{k} = 'dx';
-% x_var{k} = 'dx';
-% x_type{k} = 'log'; 
-% y_label{k} = 'error';
-% y_var{k} = 'error_L1';
-
-
-    % output = fullfile('..\\..\\dflowfmoutput\\masscon_map.nc'); 
-    % gridInfo=EHY_getGridInfo(output,'XYcen');
-    % x = gridInfo.Xcen;
-    % y = gridInfo.Ycen;
-    % dx(k) = mean(abs(diff(x)));
-    % 
-    % f0 = sol(x,t0/D,L,M0,D);
-    % f1 = sol(x,t1/D,L,M0,D);
-    % %writematrix([x,y,round(f0,15)],'inised.xyz', 'FileType','text','Delimiter',' ')
-    % 
-    % %plot(x,f0,'b-',x,f1,'b--')
-    % % D=2; 
-    % % f0 = sol(x,t0/D,L,M0,D);
-    % % f1 = sol(x,t1/D,L,M0,D);
-    % 
-    % sed_conc = ncread(output, 'mesh2d_tracer')
-    % 
-    % error_L1(k) = mean(abs(reshape(sed_conc(:,end)-f1,[],1)));
-    % error_Linf(k) = max(abs(reshape(sed_conc(:,end)-f1,[],1)));
-    % if make_each_plot
-    %     figure(1)
-    %     clf
-    %     plot(x,f0,'--','displayname', 'initial')
-    %     hold on;
-    %     plot(x,f1,'-','displayname', 'exact')
-    %     plot(x,sed_conc(:,end),'o','displayname', 'modelled')
-    %     legend; 
-    %     xlabel('distance (m)')
-    %     ylabel('concentration (-)')
-    %     box on;
-    %     grid on;
-    % 
-    %     print('-dpng', '-r300',  ['comparison_abs.png'])
-    % 
-    %     figure(2)
-    %     clf
-    %     plot(x,sed_conc(:,end)-f1,'displayname', 'modelled-exact')
-    %     xlabel('distance (m)')
-    %     ylabel('concentration difference (-)')
-    %     box on;
-    %     grid on;
-    % 
-    %     print('-dpng', '-r300',  ['comparison_diff.png'])
-    % end
-%%
-% figure(3); 
-% plot(dx, error_L1,'DisplayName','$L^1$');
-% hold on; 
-% plot(dx, error_Linf,'DisplayName','$L^\infty$');
-% leg1 = legend
-% set(leg1,'Interpreter','latex');
-% hold off;
-% xlabel('grid size (m)','Interpreter','latex')
-% ylabel('error (-)','Interpreter','latex')
-% box on
-% grid on 
-% set(gca, 'XScale','log')
-% set(gca, 'YScale','log')
-% 
-% order_Linf = log(error_Linf(1:end-1)./error_Linf(2:end))./log(dx(1:end-1)./dx(2:end))
-% order_L1 = log(error_L1(1:end-1)./error_L1(2:end))./log(dx(1:end-1)./dx(2:end))
-
-% simulation_name= '../../dflowfmoutput/masscon_mass_balances.nc';
-% %mbc =  ncread(simulation_name,'const1_balance_cumerror');
-% t =  ncread(simulation_name,'time');
-% figure(2) 
-% plot(t,mbc(end,:))
-% xlabel(ncreadatt(simulation_name,'time', "units"));
-% ylabel(sprintf('%s [%s]', ncreadatt(simulation_name,'const1_balance_cumerror', "long_name"), ncreadatt(simulation_name,'const1_balance_cumerror', "units")));
-% 
-% box on;
-% grid on; 
-% print('-dpng','-r300','mass_error.png')
-
 
 function f = sol(x,t,L,M0,D)
+    % https://math.libretexts.org/@go/page/90436 
+    % shared under CC BY 3.0 license 
+    % (https://creativecommons.org/licenses/by/3.0)
     Nf = 50; 
     a0 = 2*M0/L;
     a = zeros(Nf,1);
